d6b5831cce835118325a1d3739299b8eac15a5a0
Ella <michaella.latkovic@gmail.com>
2019-07-24 21:44:22 -0700

    added college index + show actions + views

--- app/controllers/colleges_controller.rb
+++ app/controllers/colleges_controller.rb
@@ -1,2 +1,12 @@
 class CollegesController < ApplicationController
+  before_action :require_logged_in
+
+  def index
+    @colleges = College.all
+  end
+
+  def show
+    @college = College.find_by_id(params[:id])
+  end
+
 end
--- app/models/college.rb
+++ app/models/college.rb
@@ -1,4 +1,5 @@
 class College < ApplicationRecord
   has_many :submissions
   has_many :users, through: :submissions
+ 
 end
