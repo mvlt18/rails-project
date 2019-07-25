8592c944705232b7aa1566c070bc8685e32f01eb
Ella <michaella.latkovic@gmail.com>
2019-07-24 21:45:59 -0700

    updated routes + added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :submissions
+
+
   resources :colleges
-  resources :users
+  # resources :users
 
   root 'sessions#home'
 
@@ -9,5 +10,16 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  resources :submissions, only: [:index, :new, :show, :create]
+
+  resources :users , only: [:new, :show] do
+    resources :submissions , only: [:show, :index]
+  end
+
+  resources :colleges , only: [:new, :show] do
+    resources :submissions , only: [:show, :index]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
