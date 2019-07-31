e288247416684e780e3a38f39622f77629747a8f
Ella <michaella.latkovic@gmail.com>
2019-07-31 13:14:29 -0700

    added scope methods/cleaned up

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,7 +1,12 @@
 class Submission < ApplicationRecord
   belongs_to :user
   belongs_to :college #, optional: true
+  belongs_to :status
 
-  enum status_type: [:draft, :applied, :accepted, :waitlisted, :rejected]
+  scope :recent, -> {order("submissions.updated_at DESC")}
+
+  def self.accepted
+    where(status_id: 4)
+  end
 
 end
