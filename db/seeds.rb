8a8d721dc3551b29d1fa19f063521440d0e04cb5
Ella <michaella.latkovic@gmail.com>
2019-07-31 17:17:48 -0700

    final version - fixed status datatype

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,7 @@ College.create(:name=>'Dartmouth College', :city=>'Hanover', :state=>'NH')
 College.create(:name=>'Cornell University', :city=>'Ithaca', :state=>'NY')
 
 User.create(:name=>'Emma', :email=>'emma@gmail.com', :password=>'emma', :undergrad_college_name=>'Boston University', :undergrad_major=>'Biology', :undergrad_minor=>'Chemistry', :undergrad_gpa=>3.6, :grad_college_name=>"", :grad_degree=>"", :grad_gpa=>nil, :gre_scores=>"V: 153, Q: 155, W: 5.0", :gmat_scores=>nil, :experience=>"Summer internship: worked doing literature reviews for a small nonprofit on a variety of environmental health impacts on population health, 1 yr on sorority executive board, special focus on philanthropy efforts with Ronald McDonald House Charities, 9 mo as a transplant coordinator, coordinating medical records, completing assessments and coordinating the entire process (screening to consent to recovery of tissue). Participated in a few projects with this job.")
-
+User.create(:name=>'Nate', :email=>'nate@gmail.com', :password=>'nate', :undergrad_college_name=>'Harvard University', :undergrad_major=>'Economics', :undergrad_minor=>'Math', :undergrad_gpa=>3.4, :grad_college_name=>"Harvard", :grad_degree=>"MBA", :grad_gpa=>nil, :gre_scores=>nil, :gmat_scores=>nil, :experience=>"5 years in tech industry - marketing and product management")
 
 Status.create(:name=>'Draft')
 Status.create(:name=>'Applied')
