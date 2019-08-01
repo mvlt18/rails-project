5c63d21432f60c2eaf08b585f53323d162fd6980
Ella <michaella.latkovic@gmail.com>
2019-07-31 18:34:07 -0700

    added helper methods

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,2 +1,11 @@
 module SubmissionsHelper
+
+  def deadline_date(submission)
+    submission.deadline.strftime("%d/%m/%Y") if submission.deadline.present?
+  end
+
+  def submitted_date(submission)
+    submission.date_submitted.strftime("%d/%m/%Y") if submission.date_submitted.present?
+  end
+  
 end
