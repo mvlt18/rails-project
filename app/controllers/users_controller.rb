52c759bafc5a5d499866b54f9dd4637a68c98ca1
Ella <michaella.latkovic@gmail.com>
2019-08-01 09:56:02 -0700

    added edit/update actions & user params that were lost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,15 +21,26 @@ class UsersController < ApplicationController
   #display user profile show page
   def show
     @user = User.find_by_id(params[:id])
-    #rescue code if user id does not exist, redirect to somewhere else.
+    #rescue code if user id does not exist, redirect to root path.
     redirect_to '/' if !@user
   end
 
+  #load edit form
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+
+  #update the user profile
+  def update
+    user = User.find_by_id(params[:id])
+    user.update(user_params)
+    redirect_to user_path
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :undergrad_college_name, :undergrad_major, :undergrad_minor, :undergrad_gpa, :grad_college_name, :grad_degree, :grad_gpa, :gre_scores, :gmat_scores, :experience)
   end
 
 end
