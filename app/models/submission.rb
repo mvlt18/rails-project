7a2bad134a2f17c866a1eaff6274623c28ad2501
Ella <michaella.latkovic@gmail.com>
2019-07-31 18:37:01 -0700

    add submission validation

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,12 +3,12 @@ class Submission < ApplicationRecord
   belongs_to :college #, optional: true
   belongs_to :status
 
+  validates :college_id, :program, :status_id, presence: true
+
   scope :recent, -> {order("submissions.updated_at DESC")}
 
   def self.accepted
     where(status_id: 4)
   end
-
-  
 
 end
